a5962e85f0aac456d64a03c66559d85634edc43e
Harry2005005 <119730110+Harry2005005@users.noreply.github.com>
2023-01-26 20:44:45 +0530

    #include<stdio.h> int main() {     int n,sum=0,pro=1,i;     scanf("%d",&n);     for(;n>0;)     {         i=n%10;         sum=sum+i;         pro=pro*i;         n=n/10;     }     if(sum==pro)         printf("Spy Number");     else         printf("Not Spy Number"); }

---
+++ Spy_Number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0,pro=1,i;
+    scanf("%d",&n);
+    for(;n>0;)
+    {
+        i=n%10;
+        sum=sum+i;
+        pro=pro*i;
+        n=n/10;
+    }
+    if(sum==pro)
+        printf("Spy Number");
+    else
+        printf("Not Spy Number");
+}
