dda1e68d36a94644a1c0b97718a0ac4b4194e509
Harry2005005 <119730110+Harry2005005@users.noreply.github.com>
2023-05-19 20:55:05 +0530

    #include<stdio.h> int main(){     int a,b,lcm;     scanf("%d%d",&a,&b);     lcm=(a>b)?a:b;     while(1){         if((lcm%a==0)&& (lcm%b==0)){             printf("%d",lcm);             break;         }         lcm++;     } }

--- LCM.c
+++ LCM.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
-int main() 
-{
+int main(){
     int a,b,lcm;
     scanf("%d%d",&a,&b);
     lcm=(a>b)?a:b;
-    while (1)
-    {
-        if ((lcm%a==0)&&(lcm%b== 0))
-        { printf("%d",lcm);
-        break;
+    while(1){
+        if((lcm%a==0)&& (lcm%b==0)){
+            printf("%d",lcm);
+            break;
         }
         lcm++;
     }
