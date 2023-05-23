176e7c86491b2e3a972a4cb4e17ab1795d512990
Prince Harry <119730110+Harry2005005@users.noreply.github.com>
2023-05-23 14:06:12 +0530

    #include<stdio.h> int main() {     int n,rev=0,rem,x;     scanf("%d",&n);     x=n;     while(n!=0)     {         rem=n%10;         rev=rev*10+rem;         n=n/10;         }         if(x==rev)         {             printf("True");         }         else         {             printf("False");         } }

--- Palindrome.c
+++ Palindrome.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int n,rev=0,x,rem;
+    int n,rev=0,rem,x;
     scanf("%d",&n);
     x=n;
     while(n!=0)
@@ -9,13 +9,13 @@ int main()
         rem=n%10;
         rev=rev*10+rem;
         n=n/10;
-    }
-    if(x==rev)
-    {
-        printf("True");
-    }
-    else 
-    {
-        printf("False");
-    }
+        }
+        if(x==rev)
+        {
+            printf("True");
+        }
+        else
+        {
+            printf("False");
+        }
 }
